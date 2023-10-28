c8dd2f06f584b7d1076cf3bb0b5e64841e431a21
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-28 15:47:36 +0300

    refactor: restructure backend folder

--- backend/kenyapix/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class CreatorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Creators
-        fields = ['name', 'email','password']
