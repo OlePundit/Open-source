7f696b26ad7c60262c6440966e82683233bcc508
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-31 03:57:48 +0300

    feat[authentication]: create user profile on account creation
    create welcome notification for new user
    
    removed unique username constraint

--- backend/apps/authentication/apps.py
+++ backend/apps/authentication/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
+
+    def ready(self) -> None:
+        try:
+            import apps.authentication.signals  # noqa: F401
+        except ImportError:
+            pass
