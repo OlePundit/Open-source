687d83b865864ebe13c43de8724eed8253f2acad
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-11-02 02:02:59 +0300

    revert: user jwt stateful authentication

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -111,7 +111,7 @@ CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
