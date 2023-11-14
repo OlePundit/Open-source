9a1061f4869143fb1f549508c4a2b62ce37a73cb
kabiruH <kelkab46@gmail.com>
2023-11-14 22:05:03 +0300

    added login

---
+++ front-end/src/Components/Login/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+
+export const Login = () => {
+ 
+ 
+ 
+ 
+ 
+    return (
+    <div className="desktop">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="rectangle" />
+          <img className="element" alt="Element" src="101138562-567171750657246-3564414549426602450-n-1.png" />
+          <div className="div" />
+          <div className="frame">
+            <div className="text-wrapper">AfroPix</div>
+            <div className="text-wrapper-2">remember me</div>
+            <p className="not-a-member-sign-up">Not a Member? Sign Up&nbsp;&nbsp;Here</p>
+            <div className="text-wrapper-3">OR</div>
+            <div className="text-wrapper-4">forgot password ?</div>
+            <div className="text-wrapper-5">LOG IN</div>
+            <div className="text-wrapper-6">EMAIL</div>
+            <div className="text-wrapper-7">PASSWORD</div>
+            <div className="frame-2" />
+            <div className="frame-3" />
+            <div className="group">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">Log In</div>
+                <div className="rectangle-2" />
+              </div>
+            </div>
+            <div className="rectangle-3" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-9">log in with facebook</div>
+                <div className="rectangle-4" />
+              </div>
+            </div>
+            <div className="group-2">
+              <div className="overlap-2">
+                <div className="text-wrapper-10">log in with facebook</div>
+                <div className="rectangle-4" />
+              </div>
+              <div className="div-wrapper">
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-10">log in with facebook</div>
+                  <div className="rectangle-4" />
+                </div>
+              </div>
+            </div>
+            <img className="vector" alt="Vector" src="vector-1.svg" />
+            <img className="img" alt="Vector" src="vector-2.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
